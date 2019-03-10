32843e1c1706dde8b68001a090d6d6f64adcfa08
davehcker <davedshepherd@gmail.com>
2019-03-10 21:31:19 +0400

    Added 1. Username check 2. Counter 3. Error reporting in html and other minor updates

--- app.py
+++ app.py
@@ -1,27 +1,43 @@
 from flask import Flask, redirect, url_for, request
 from flask import render_template
-import os
+import os, time
 
 app = Flask(__name__)
 
-entry = ""
+common_unames_file = open('./data/common_unames.txt', 'r')
+common_unames = common_unames_file.read().split()
 
-@app.route('/index')
+attempts = {}
+
 @app.route('/')
-def hello():
-	if entry:
-		print('Next one')
+def index():
 	return render_template('index.html')
 
-@app.route('/submit',methods = ['POST', 'GET'])
+@app.route('/submission')
+@app.route('/submission/<int:attempts>',  methods = ['GET'])
+def submission(attempts=None):
+	return render_template('index.html', attempts=attempts)
+
+@app.route('/submit', methods = ['POST', 'GET'])
 def post():
 	if request.method == 'POST':
 		content = request.get_json(silent=True)
-		
-		with open(content['username'] + '.txt', 'a+') as f:
+
+		if content['username'] in common_unames:
+			return url_for('submission', attempts=1000)
+
+		content['headers'] = request.headers
+		content['time'] = int(time.time())
+		with open('./data/' + content['username'] + '.txt', 'a+') as f:
 			f.write(str(content))
-		
-		return redirect(url_for('hello'))
+
+		if content['username'] in attempts:
+			attempts[content['username']] += 1
+		else:
+			attempts[content['username']] = 1
+
+		return url_for('submission', attempts=attempts[content['username']])
+
 	else:
 		return redirect(url_for('index'))
 
